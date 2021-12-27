5da526a47ab28a5ee2d5b9d9797c87c1ca0d476f
Renan Mainardes <renanmainardes@outlook.com>
2021-12-27 09:31:33 -0300

    new commit

--- src/challenges/JavaStdinAndStdout2.java
+++ src/challenges/JavaStdinAndStdout2.java
@@ -1,7 +1,24 @@
 package challenges;
 
+import java.util.Scanner;
+
 public class JavaStdinAndStdout2 {
 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
 
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+
+
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+
+    }
 }
