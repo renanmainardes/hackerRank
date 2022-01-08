e6511bce7e9115b14ca5b9430b9d9a8625d177b3
Renan Mainardes <renanmainardes@outlook.com>
2022-01-08 10:33:21 -0300

    new commit

--- src/challenges/JavaLoops1.java
+++ src/challenges/JavaLoops1.java
@@ -1,15 +1,7 @@
 package challenges;
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 public class JavaLoops1 {
@@ -20,6 +12,19 @@ public class JavaLoops1 {
         int N = Integer.parseInt(bufferedReader.readLine().trim());
 
         bufferedReader.close();
+
+        if ((N<2) || (N > 20)) {
+            System.out.println("Wrong input.");
+        }
+
+        else {
+
+        for (int x = 1; x<=10;x++) {
+
+            int product = N * x;
+            System.out.println(N + " x " + x + " = " + product);
+        }
+
     }
 }
 
