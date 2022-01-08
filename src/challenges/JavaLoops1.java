5498654e7fff9d5fb48e9c3929c865777ef31601
Renan Mainardes <renanmainardes@outlook.com>
2022-01-08 09:36:30 -0300

    new commit

---
+++ src/challenges/JavaLoops1.java
@@ -0,0 +1,26 @@
+package challenges;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+public class JavaLoops1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+    }
+}
+
+}
