5b530a014df40c4033991c77bb8776ee3fda004c
Renan Mainardes <renanmainardes@outlook.com>
2022-01-28 10:39:20 -0300

    new commit

--- src/challenges/JavaIntToString.java
+++ src/challenges/JavaIntToString.java
@@ -5,7 +5,7 @@ public class JavaIntToString {
     public static void main(String[] args) {
 
         int n = 0; //INT N É A VARIÁVEL INSERIDA PELO USUÁRIO, JÁ PARTE DO PROGRAMA INICIAL
-        String s = Integer.toString(n);
+        String s = Integer.toString(n); //A SOLUÇÃO É APENAS ESTA LINHA!
 
     }
 
